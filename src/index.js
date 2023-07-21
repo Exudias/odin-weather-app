6ea60f990d290ea49cc86d6f8a08639c551db49c
Exudias <editoo31@gmail.com>
2023-07-21 13:25:38 +0300

    Track API key, create API access function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-import './styles/style.css';
+import "./styles/style.css";
+
+const API_KEY = "8ba4e980a5c44bdfbb981113232107"; // it's a free key, I know it's not secured
+
+async function getLocationData(location) {
+  try {
+    const response = await fetch(
+      `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${location}&aqi=no`,
+      { mode: "cors" }
+    );
+
+    if (!response.ok) {
+      throw new Error(`HTTP Error! Status: ${response.status}`);
+    }
+
+    const json = await response.json();
+    return json;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
