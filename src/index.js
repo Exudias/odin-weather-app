b0c75d79471de441cd1211d495b0d84c6ccc82a7
Exudias <editoo31@gmail.com>
2023-07-21 14:13:43 +0300

    Parse JSON into an object of relevant info

--- src/index.js
+++ src/index.js
@@ -21,4 +21,55 @@ async function getLocationData(location, days) {
   }
 }
 
-console.log(await getLocationData("London", 2));
+function objectFromWeatherJSON(json) {
+  let obj = {};
+
+  obj.name = json.location.name;
+
+  obj.currentTempC = json.current.temp_c;
+  obj.currentTempF = json.current.temp_f;
+
+  obj.feelsLikeC = json.current.feelslike_c;
+  obj.feelsLikeF = json.current.feelslike_f;
+
+  obj.currentCondition = {
+    icon: json.current.condition.icon,
+    text: json.current.condition.text,
+  };
+
+  obj.currentWindKPH = json.current.wind_kph;
+  obj.currentWindMPH = json.current.wind_mph;
+
+  obj.forecastDays = json.forecast.forecastday.map((day) =>
+    parseForecastDayJSON(day)
+  );
+
+  return obj;
+}
+
+function parseForecastDayJSON(json) {
+  let obj = {};
+
+  obj.date = json.date;
+
+  obj.averageTempC = json.day.avgtemp_c;
+  obj.averageTempF = json.day.avgtemp_f;
+
+  obj.condition = {
+    icon: json.day.condition.icon,
+    text: json.day.condition.text
+  };
+
+  obj.chanceOfRain = json.day.daily_chance_of_rain;
+  obj.chanceOfSnow = json.day.daily_chance_of_snow;
+
+  obj.highC = json.day.maxtemp_c;
+  obj.highF = json.day.maxtemp_f;
+
+  obj.lowC = json.day.mintemp_c;
+  obj.lowF = json.day.mintemp_f;
+  
+  return obj;
+}
+
+console.log(objectFromWeatherJSON(await getLocationData("London", 3)));
